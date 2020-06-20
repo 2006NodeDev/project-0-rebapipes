fd26fd277a9af1bf91389211f6c0282f5a9a4fe3
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-06-20 18:58:14 -0500

    added invalidcredentialserror.ts to errors

--- Expense Reimbursement System (ERS) API/src/errors/HttpError.ts
+++ Expense Reimbursement System (ERS) API/src/errors/HttpError.ts
@@ -1,6 +1,6 @@
 export abstract class HttpError extends Error {
-    statusCode:number//we extended to add on a new field to error
-    constructor(statusCode:number, message?:string){
+    statusCode:number
+    constructor(statusCode:number, message?:string) {
         super(message)
         this.statusCode = statusCode
     }
