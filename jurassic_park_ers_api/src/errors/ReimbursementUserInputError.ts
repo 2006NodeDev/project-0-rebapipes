92b58aca30ce9f73d7f306b6669bd4eaadd13bb0
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-06-25 19:47:06 -0500

    added reimbursementidinputerror.ts, reimbursementnotfounderror.ts, and reimbursementuserinputerror.ts

---
+++ jurassic_park_ers_api/src/errors/ReimbursementUserInputError.ts
@@ -0,0 +1,8 @@
+  
+import { HttpError } from "./HttpError";
+
+export class ReimbursementUserInputError extends HttpError {
+    constructor(){
+        super(400, 'Please Fill Out All Reimbursemet Fields')
+    }
+}
