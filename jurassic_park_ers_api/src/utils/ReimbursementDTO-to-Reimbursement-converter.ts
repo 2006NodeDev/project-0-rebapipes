4220dcf7e2d00d7ae8c983e49501be210db55606
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-06-26 11:40:34 -0500

    fixed syntax error(s) in reimbursement-dto.ts

---
+++ jurassic_park_ers_api/src/utils/ReimbursementDTO-to-Reimbursement-converter.ts
@@ -0,0 +1,23 @@
+import { ReimbursementDTO } from "../dtos/reimbursement-dto";
+import { Reimbursement } from "../models/Reimbursement";
+
+// works perfectly with the map function
+export function BookDTOtoBookConvertor( bto:BookDTO):Book{
+    let genre:Genre[] = [];
+    for(const g of bto.genres){
+        genre.push({genreId:0, genre:g})// this si a problem to solve in the future
+    }
+    return {
+        ISBN: bto.ISBN.toString(),
+        authors:bto.authors,
+        genre,
+        bookId:bto.book_id,
+        chapters:bto.chapters,
+        pages:bto.pages,
+        numberInSeries:bto.number_in_series,
+        publisher:bto.publisher,
+        publishingDate: bto.publishing_date.getFullYear(),
+        series:bto.series,
+        title:bto.title
+    }
+}
