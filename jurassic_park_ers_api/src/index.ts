d4423991d3f9d94deadb1a9b988364f2d96be0cf
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-06-27 18:32:12 -0500

    updated index.ts in src

--- jurassic_park_ers_api/src/index.ts
+++ jurassic_park_ers_api/src/index.ts
@@ -1,9 +1,13 @@
-import express, { Request, Response } from 'express'
-import { userRouter, users } from './routers/user-router'
+import express, { Request, Response, NextFunction } from 'express'
+import { userRouter } from './routers/user-router';
 import { reimbursementRouter } from './routers/reimbursement-router'
+import { LoginInvalidCredentialsError } from './errors/LoginInvalidCredentialsError'
+import { getUserByUsernameAndPassword } from './daos/user-dao';
 import { loggingMiddleware } from './middleware/logging-middleware'
 import { sessionMiddleware } from './middleware/session-middleware'
-import { LoginInvalidCredentialsError } from './errors/LoginInvalidCredentialsError'
+import { authorizationMiddleware } from './middleware/authorization-middleware'
+import { authenticationMiddleware } from './middleware/authentication-middleware'
+
 import { AuthenticationError } from './errors/AuthenticationError'
 import { AuthorizationError } from './errors/AuthorizationError'
 import { LoginUserInputError } from './errors/LoginUserInputError'
@@ -15,40 +19,46 @@ const app = express()
 app.use(express.json())
 
 app.use(loggingMiddleware)
+
 app.use(sessionMiddleware)
 
-app.use('/reimbursements', reimbursementRouter)
+//app.use(authorizationMiddleware)
+
+//app.use(authenticationMiddleware)
+
 app.use('/users', userRouter)
 
-app.post('/login', (req:Request, res:Response)=>{
+app.use('/reimbursements', reimbursementRouter)
+
+app.post('/login', async (req:Request, res:Response, next:NextFunction)=>{
+    // you could use destructuring, see ./routers/book-router
     let username = req.body.username
     let password = req.body.password
+    // if I didn't get a usrname/password send an error and say give me both fields
     if(!username || !password){
+        // make a custom http error and throw it or just send a res
         throw new LoginInvalidCredentialsError()
     } else {
-        let found = false
-        for(const user of users) {
-            if(user.username === username && user.password === password){
-                req.session.user = user
-                res.json(user)
-                found = true
-            }
-        }
-        if(!found){
-            throw new AuthorizationError()
+        try{
+            let user = await getUserByUsernameAndPassword(username, password)
+            req.session.user = user// need to remeber to add their user data to the session
+            // so we can use that data in other requests
+            res.json(user)
+        }catch(e){
+            next(e)
         }
     }
 })
 
-app.use((err, req, res, next) => {
-    if (err.statusCode) {
+app.use((err, req, res, next) =>{
+    if (err.statusCode){
         res.status(err.statusCode).send(err.message)
-    } else {
+    }else{
         console.log(err)
         res.status(500).send('Oops, Something Went Wrong')
     }
 })
 
 app.listen(2006, () => {
-    console.log('Server has started');
+    console.log("Server Is Running");
 })
