67b17916fc8077c3fbd0c34571d9b0b8a0e8707e
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-06-30 08:29:41 -0500

    updated reimbursement dao; changed reimbusementidinputerror to reimbursementinputerror; deleted reimbursement-author & reimbursement-status dao

--- jurassic_park_ers_api/src/daos/reimbursement-dao.ts
+++ jurassic_park_ers_api/src/daos/reimbursement-dao.ts
@@ -3,8 +3,8 @@ import { connectionPool } from ".";
 import { Reimbursement } from "../models/Reimbursement";
 import { ReimbursementDTOtoReimbursementConverter } from "../utils/ReimbursementDTO-to-Reimbursement-converter";
 import { ReimbursementNotFoundError } from "../errors/ReimbursementNotFoundError";
+import { ReimbursementInputError } from "../errors/ReimbursementInputError";
 //import { UserInputError } from "../errors/UserInputError";
-//import { ReimbursementIdInputError } from "../errors/ReimbursementIdInputError";
 //import { AuthorizationError } from '../errors/AuthorizationError'
 
 // Get all Reimbursements
@@ -32,67 +32,192 @@ export async function getAllReimbursements():Promise<Reimbursement[]>{
     }
 }
 
-// Get Reimbursements by Id
+// Get Reimbursement by User Id
 
-export async function getReimbursementsById(id:number):Promise<Reimbursement>{
-    let client:PoolClient;
+export async function getReimbursementByUserId(userId:number):Promise<Reimbursement[]> {
+    let client:PoolClient
     try {
-        client = await connectionPool.connect();
-        let results:QueryResult = await client.query(`select r.*, rs.status, rs."status_id", rt."type", rt."type_id" from jurassic_park_ers_api.reimbursement r
-                                                        join jurassic_park_ers_api.reimbursement_status rs on r.status = rs.status_id
-                                                        join jurassic_park_ers_api.reimbursement_type rt on r."type" = rt.type_id
-                                                        where r.reimbursement_id = $1;`, [id]);
-        return ReimbursementDTOtoReimbursementConverter(results.rows[0]);
-    } catch (error) {
-        if (error.message === "Reimbursement Not Found"){
-            console.log(error);
+        client = await connectionPool.connect()
+        let results = await client.query(`select r."reimbursement_id", 
+                                                r."author", r."amount", 
+                                                r."date_submitted",
+                                                r."date_resolved",
+                                                r."description", r."resolver",
+                                                rs."status_id", rs."status",
+                                                rt."type_id", rt."type"
+                                            from jurassic_park_ers_api.reimbursements r 
+                                            left join jurassic_park_ers_api.reimbursement_statuses rs
+                                                on r."status" = rs."status_id" 
+                                            left join jurassic_park_ers_api.reimbursement_types rt
+                                                on r."type" = rt."type_id"
+                                            left join jurassic_park_ers_api.users u 
+                                                on r."author" = u."user_id"
+                                                    where u."user_id" = $1
+                                            order by r.date_submitted;`, [userId])
+        if(results.rowCount === 0) {
+            throw new Error('Reimbursement Not Found')
+        }
+        return results.rows.map(ReimbursementDTOtoReimbursementConverter);
+    } catch (e) {
+        if(e.message === 'Reimbursement Not Found') {
             throw new ReimbursementNotFoundError()
         }
-        throw new Error('An Unknown Error Occurred');
+        console.log(e);
+        throw new Error('An Unknown Error Occurred')
     } finally {
-        client && client.release();
+        client && client.release()
     }
 }
 
-// Save Reimbursement
-export async function saveOneReimbursement(newReimbursement:Reimbursement):Promise<Reimbursement>{
+// Get Reimbursement By Status
+export async function getReimbursementByStatus(status:number):Promise<Reimbursement[]> {
     let client:PoolClient
-    try{
+    try {
         client = await connectionPool.connect()
-        let results = await client.query(`insert into jurassic_park_ers_api.reimbursement ("author",
-                                            "amount","date_submitted","description","status","type")
-                                            values($1,$2,$3,$4,$5,$6) returning "reimbursement_id" `,
-                                            [newReimbursement.author, newReimbursement.amount, newReimbursement.dateSubmitted, 
-                                            newReimbursement.description, 1, newReimbursement.type.typeId]);
-        newReimbursement.reimbursementId = results.rows[0].reimbursement_id
-        console.log("New Id: ", newReimbursement.reimbursementId)
-        return newReimbursement
-    }catch(e){
-        console.log(e)
+        let results = await client.query(`select r."reimbursement_id", 
+                                                r."author", 
+                                                r."amount", 
+                                                r."date_submitted",
+                                                r."date_resolved",
+                                                r."description",
+                                                r."resolver",
+                                                rs."status_id", 
+                                                rs."status",
+                                                rt."type_id",
+                                                rt."type"
+                                                    from jurassic_park_ers_api.reimbursements r 
+                                            left join jurassic_park_ers_api.reimbursement_statuses rs
+                                                on r."status" = rs."status_id" 
+                                            left join jurassic_park_ers_api.reimbursement_types rt
+                                                on r."type" = rt."type_id"
+                                                    where r."status" = $1
+                                            order by r.date_submitted;`, [status])
+        if(results.rowCount === 0) {
+            throw new Error('Reimbursement Not Found')
+        }
+        return results.rows.map(ReimbursementDTOtoReimbursementConverter);
+    } catch (e) {
+        if(e.message === 'Reimbursement Not Found') {
+            throw new ReimbursementNotFoundError()
+        }
+        console.log(e);
         throw new Error('An Unknown Error Occurred')
-    }finally{
-        client && client.release();
+    } finally {
+        client && client.release()
+    }
+}
+
+// Save (Create) Reimbursement
+export async function saveOneReimbursement(newReim:Reimbursement):Promise<Reimbursement> {
+    let client:PoolClient
+    try {
+        client = await connectionPool.connect()
+        await client.query('BEGIN;')
+        let typeId = await client.query(`select t."type_id" from jurassic_park_ers_api.reimbursement_types t 
+                                            where t."type" = $1;`,
+                                        [newReim.type])
+        if(typeId.rowCount === 0) {
+            throw new Error('Type Not Found')
+        }
+        typeId = typeId.rows[0].type_id 
+        let statusId = await client.query(`select rs."status_id" from jurassic_park_ers_api.reimbursement_statuses rs 
+                                            where rs."status" = $1;`, [newReim.status])
+        if(statusId.rowCount === 0) {
+            throw new Error('Status Not Found')
+        }
+        statusId = statusId.rows[0].status_id
+        let results = await client.query(`insert into jurassic_park_ers_api.reimbursements ("author", "amount", 
+                                        "date_submitted", "description", "status", "type")
+                                            values($1,$2,$3,$4,$5,$6) 
+                                        returning "reimbursement_id";`,
+                                        [newReim.author, newReim.amount, newReim.dateSubmitted,
+                                            newReim.description, statusId, typeId]) 
+        newReim.reimbursementId = results.rows[0].reimbursement_id
+        
+        await client.query('COMMIT;')
+        return newReim
+    } catch (e) {
+        client && client.query('ROLLBACK;')
+        if(e.message === 'Type Not Found' || e.message === 'Status Not Found') {
+            throw new ReimbursementInputError()
+        } 
+        console.log(e);
+        throw new Error('An Unknown Error Occurred')
+    } finally {
+        client && client.release()
     }
 }
 
 // Update Reimbursement
-export async function updateOneReimbursement(updatedReimbursement:Reimbursement):Promise<Reimbursement>{
+export async function updateReimbursement(updatedReimbursement:Reimbursement):Promise<Reimbursement> {
     let client:PoolClient
-    try{
+    try {
         client = await connectionPool.connect()
-        await client.query(`update jurassic_park_ers_api.reimbursement as r
-                                set "author"=$1, "amount"=$2, "date_submitted"=$3, "date_resolved"=$4, "description"=$5, 
-                                "resolver"=$6, "status"=$7, "type"=$8
-                                where r."reimbursement_id"=$9 returning "reimbursement_id"`,
-                                [updatedReimbursement.author, updatedReimbursement.amount, updatedReimbursement.dateSubmitted,
-                                updatedReimbursement.dateResolved, updatedReimbursement.description, updatedReimbursement.resolver,
-                                updatedReimbursement.status.statusId, updatedReimbursement.type.typeId, updatedReimbursement.reimbursementId]);
-        
-        return getReimbursementsById(updatedReimbursement.reimbursementId);
-    }catch(e){
-        console.log(e)
-        throw new Error('Cannot Process Update')
-    }finally{
-        client && client.release();
+        await client.query('BEGIN;')
+
+        if(updatedReimbursement.author) {
+            await client.query(`update jurassic_park_ers_api.reimbursements set "author" = $1 
+                                where "reimbursement_id" = $2;`, 
+                                [updatedReimbursement.author, updatedReimbursement.reimbursementId])
+        }
+        if(updatedReimbursement.amount) {
+            await client.query(`update jurassic_park_ers_api.reimbursements set "amount" = $1 
+                                where "reimbursement_id" = $2;`, 
+                                [updatedReimbursement.amount, updatedReimbursement.reimbursementId])
+        }
+        if(updatedReimbursement.dateSubmitted) {
+            await client.query(`update jurassic_park_ers_api.reimbursements set "date_submitted" = $1 
+                                where "reimbursement_id" = $2;`, 
+                                [updatedReimbursement.dateSubmitted, updatedReimbursement.reimbursementId])
+        }
+        if(updatedReimbursement.dateResolved) {
+            await client.query(`update jurassic_park_ers_api.reimbursements set "date_resolved" = $1 
+                                where "reimbursement_id" = $2;`, 
+                                [updatedReimbursement.dateResolved, updatedReimbursement.reimbursementId])
+        }
+        if(updatedReimbursement.description) {
+            await client.query(`update jurassic_park_ers_api.reimbursements set "description" = $1 
+                                where "reimbursement_id" = $2;`, 
+                                [updatedReimbursement.description, updatedReimbursement.reimbursementId])
+        }
+        if(updatedReimbursement.resolver) {
+            await client.query(`update jurassic_park_ers_api.reimbursements set "resolver" = $1 
+                                where "reimbursement_id" = $2;`, 
+                                [updatedReimbursement.resolver, updatedReimbursement.reimbursementId])
+        }
+        if(updatedReimbursement.status) {
+            let statusId = await client.query(`select rs."status_id" from jurassic_park_ers_api.reimbursement_statuses rs 
+                                            where rs."status" = $1;`, [updatedReimbursement.status])
+            if(statusId.rowCount === 0) {
+                throw new Error('Status Not Found')
+            }
+            statusId = statusId.rows[0].status_id
+            await client.query(`update jurassic_park_ers_api.reimbursements set "status" = $1 
+                                where "reimbursement_id" = $2;`, 
+                                [statusId, updatedReimbursement.reimbursementId])
+        }
+        if(updatedReimbursement.type) {
+            let typeId = await client.query(`select rt."type_id" from jurassic_park_ers_api.reimbursement_types rt 
+                                            where rt."type" = $1;`, [updatedReimbursement.type])
+            if(typeId.rowCount === 0) {
+                throw new Error('Type Not Found')
+            }
+            typeId = typeId.rows[0].type_id
+            await client.query(`update jurassic_park_ers_api.reimbursements set "type" = $1 
+                                where "reimbursement_id" = $2;`, 
+                                [typeId, updatedReimbursement.reimbursementId])
+        }
+
+        await client.query('COMMIT;')
+        return updatedReimbursement
+    } catch(e) {
+        client && client.query('ROLLBACK;')
+        if(e.message == 'Status Not Found' || e.message == 'Type Not Found') {
+            throw new ReimbursementInputError()
+        }
+        console.log(e);
+        throw new Error('An Unknown Error Occurred')
+    } finally {
+        client && client.release()
     }
 }
--- jurassic_park_ers_api/src/errors/ReimbursementIdInputError.ts
+++
@@ -1,7 +0,0 @@
-import { HttpError } from "./HttpError";
-
-export class ReimbursementIdInputError extends HttpError{
-    constructor(){
-        super(400, 'Reimbursement ID must be a number')
-    }
-}
