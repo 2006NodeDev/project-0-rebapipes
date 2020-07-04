47777b872d5ac996a6454e979eeae33ea583a8a0
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-07-04 10:27:28 -0500

    overhauled reimbursement status router

--- jurassic_park_ers_api/src/routers/reimbursement-status-router.ts
+++ jurassic_park_ers_api/src/routers/reimbursement-status-router.ts
@@ -1,15 +1,14 @@
-import express, { Request, Response, NextFunction } from 'express';
-import { getReimbursementByStatus } from '../daos/reimbursement-dao';
-//import { authorizationMiddleware } from '../middleware/authorization-middleware';
+import express, { Request, Response, NextFunction } from 'express'
+import { getReimbursementByStatus } from '../daos/reimbursement-dao'
+import { authorizationMiddleware } from '../middleware/authorization-middleware'
 
-export const reimbursementStatusRouter = express.Router();
+export const reimbursementStatusRouter = express.Router()
 
-// Get Reimbursement by Status
-
-reimbursementStatusRouter.get('/:statusId', async (req:Request, res:Response, next:NextFunction) => {
+//Find Reimbursement By Status 
+reimbursementStatusRouter.get('/:statusId', authorizationMiddleware(['Admin', 'Finance Manager']), async (req:Request, res:Response, next:NextFunction) => {
     let { statusId } = req.params
     if(isNaN(+statusId)) {
-        res.status(400).send('Id must be a number')
+        res.status(400).send('Status Id must be a number')
     }
     else { 
         try {
