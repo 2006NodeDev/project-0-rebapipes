06e16950fbedf491fa6c9da340e168740bf45148
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-07-04 10:15:59 -0500

    overhauled user dto converter

--- jurassic_park_ers_api/src/utils/UserDTO-to-User-converter.ts
+++ jurassic_park_ers_api/src/utils/UserDTO-to-User-converter.ts
@@ -1,17 +1,18 @@
-import { User } from "../models/User";
-import { UserDTO } from "../dtos/User-dto";
+import { UserDTO } from "../dtos/user-dto";
+import { User } from "../models/user";
 
-export function UserDTOtoUserConverter( udto:UserDTO ):User{
+//UserDTO takes objects in DB format & converts it to User model object(s)
+export function UserDTOtoUserConverter(udto: UserDTO): User {
     return {
         userId: udto.user_id,
-        username: udto.username, 
+        username: udto.username,
         password: udto.password,
         firstName: udto.first_name,
-        lastName: udto.last_name, 
-        email: udto.email, 
-        role:{ 
-            role:udto.role, 
-            roleId:udto.role_id 
+        lastName: udto.last_name,
+        email: udto.email,
+        role: {
+            role: udto.role,
+            roleId: udto.role_id
         }
     }
 }
