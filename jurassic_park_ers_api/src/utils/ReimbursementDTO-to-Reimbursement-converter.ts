6828fac8b49098a1f372a617b91c797b9a72e50c
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-07-04 10:18:05 -0500

    updated reimbursement dto converter - now has 6 errors

--- jurassic_park_ers_api/src/utils/ReimbursementDTO-to-Reimbursement-converter.ts
+++ jurassic_park_ers_api/src/utils/ReimbursementDTO-to-Reimbursement-converter.ts
@@ -1,16 +1,23 @@
-import { Reimbursement } from "../models/Reimbursement";
 import { ReimbursementDTO } from "../dtos/reimbursement-dto";
+import { Reimbursement } from "../models/reimbursement";
 
+//ReimbursementDTO takes objects in DB format & converts it to Reimbursement model object(s)
 export function ReimbursementDTOtoReimbursementConverter(rdto: ReimbursementDTO): Reimbursement {
-  return {
-    reimbursementId: rdto.reimbursement_id,
-    author: rdto.author,
-    amount: rdto.amount,
-    dateSubmitted: rdto.date_submitted.getFullYear(),
-    dateResolved: rdto.date_resolved.getFullYear(),
-    description: rdto.description,
-    resolver: rdto.resolver,
-    status: rdto.status,
-    type: rdto.type,
-  };
+    return {
+        reimbursementId: rdto.reimbursement_id,
+        author: rdto.author,
+        amount: rdto.amount,
+        dateSubmitted: new Date(rdto.date_submitted), //rdto.date_submitted.getFullYear(),
+        dateResolved: new Date(rdto.date_resolved),//rdto.date_resolved.getFullYear(),
+        description: rdto.description,
+        resolver: rdto.resolver,
+        status: {
+            status: rdto.status,
+            statusId: rdto.status_id
+        },
+        type: {
+            type: rdto.type,
+            typeId: rdto.type_id
+        }
+    }
 }
