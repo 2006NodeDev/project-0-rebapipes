43095794d1a7293c140bcba1c5dc8e9cb027545d
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-07-04 10:13:58 -0500

    overhauled index.ts

--- jurassic_park_ers_api/src/index.ts
+++ jurassic_park_ers_api/src/index.ts
@@ -1,51 +1,50 @@
 import express, { Request, Response, NextFunction } from 'express'
-import { userRouter } from './routers/user-router';
-import { reimbursementRouter } from './routers/reimbursement-router'
-import { loggingMiddleware } from './middleware/logging-middleware'
 import { sessionMiddleware } from './middleware/session-middleware'
-import { getByUsernameAndPassword } from './daos/user-dao';
+import { userRouter } from './routers/user-router'
+import { reimbursementRouter } from './routers/reimbursement-router'
+import { loginByUsernameAndPassword } from './daos/user-dao'
 import { AuthenticationError } from './errors/AuthenticationError'
-//import { LoginInvalidCredentialsError } from './errors/LoginInvalidCredentialsError'
+import { loggingMiddleware } from './middleware/logging-middleware'
 
-const app = express()
+const app = express() //Creates complete express application
+app.use(express.json()) //Matches every HTTP verb, middleware
+app.use(loggingMiddleware) //Logs out request method, ip address making request, and path of request
+app.use(sessionMiddleware) //Attaches a session object to the request where each unique connection to the server has a unique session
+app.use('/users', userRouter) //Redirect all requests on /users to user-router
+app.use('/reimbursements', reimbursementRouter) //Redirect all requests on /reimbursements to reimbursement-router
 
-app.use(express.json())
-app.use(loggingMiddleware)
-app.use(sessionMiddleware)
-
-app.use('/users', userRouter)
-app.use('/reimbursements', reimbursementRouter)
-
-// Login
-
+// Login 
 app.post('/login', async (req:Request, res:Response, next:NextFunction) => {
-  let username = req.body.username
-  let password = req.body.password
+    let username = req.body.username
+    let password = req.body.password
 
-  if(!username || !password) {
-      throw new AuthenticationError();
-  }
-  else { 
-      try {
-          let user = await getByUsernameAndPassword(username, password)
-          req.session.user = user
-          res.json(user)
-      } catch (e) {
-          next(e)
-      }
-  }
+    if(!username || !password) {
+        throw new AuthenticationError();
+    }
+    else { 
+        try {
+            let user = await loginByUsernameAndPassword(username, password)
+            req.session.user = user
+            res.json(user)
+        } catch (e) {
+            next(e)
+        }
+    }
 })
 
+
+// Error Handling
 app.use((err, req, res, next) => {
-  if(err.statusCode) { 
-      res.status(err.statusCode).send(err.message) 
-  }
-  else { 
-      console.log(err);
-      res.status(500).send('Oops, something went wrong')
-  }
+    if(err.statusCode) { //if it's one of my custom HTTP errors
+        res.status(err.statusCode).send(err.message) //send custom error
+    }
+    else { //not ready for this specific error, debug whatever comes out here
+        console.log(err);
+        res.status(500).send('Oops, something went wrong')
+    }
 })
 
+//Set port for sending/receiving requests
 app.listen(2006, () => {
-    console.log("Server Is Running");
+    console.log('Server Is Running');
 })
