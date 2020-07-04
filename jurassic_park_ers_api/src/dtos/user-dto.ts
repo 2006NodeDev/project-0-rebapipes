93c29a8e3bc15ada35d43ffb6829b4f2d36204f9
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-07-04 11:04:34 -0500

    overhauled reimbursement and user dtos

--- jurassic_park_ers_api/src/dtos/user-dto.ts
+++ jurassic_park_ers_api/src/dtos/user-dto.ts
@@ -1,9 +1,11 @@
+// User data from DB
+
 export class UserDTO {
   user_id: number
-  username: string
-  password: string
-  first_name: string
-  last_name: string
+    username: string
+    password: string
+    first_name: string
+    last_name: string
   email: string
   role_id: number
   role: string
